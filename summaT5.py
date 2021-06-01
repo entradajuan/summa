6b91cb4c3530dc435f58d434b8fe2181a737d89c
entradajuan <entradajuan@yahoo.es>
2021-06-01 17:20:23 +0000

    developing a basic summarizer

--- summaT5.py
+++ summaT5.py
@@ -11,3 +11,21 @@ from transformers import T5Tokenizer, T5ForConditionalGeneration, T5Config
 model = T5ForConditionalGeneration.from_pretrained('t5-large')
 tokenizer = T5Tokenizer.from_pretrained('t5-large')
 device = torch.device('cpu')
+
+def summarize(text,ml):
+  preprocess_text = text.strip().replace("\n","")
+  t5_prepared_Text = "summarize: "+preprocess_text
+  print ("Preprocessed and prepared text: \n", t5_prepared_Text)
+
+  tokenized_text = tokenizer.encode(t5_prepared_Text, return_tensors="pt").to(device)
+
+  # summmarize 
+  summary_ids = model.generate(tokenized_text,
+                                      num_beams=4,
+                                      no_repeat_ngram_size=2,
+                                      min_length=30,
+                                      max_length=ml,
+                                      early_stopping=True)
+
+  output = tokenizer.decode(summary_ids[0], skip_special_tokens=True)
+  return output
