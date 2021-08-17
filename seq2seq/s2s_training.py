45879bd2649f4fce56eadd6b490c93173428e063
entradajuan <entradajuan@yahoo.es>
2021-08-17 12:04:50 +0000

    model creation

--- seq2seq/s2s_training.py
+++ seq2seq/s2s_training.py
@@ -257,3 +257,118 @@ class Decoder(tf.keras.Model):
 encoder = Encoder(vocab_size, embedding_dim, units, BATCH_SIZE)
 decoder = Decoder(vocab_size, embedding_dim, units, BATCH_SIZE)
 
+# Learning rate scheduler
+lr_schedule = tf.keras.optimizers.schedules.InverseTimeDecay(
+                  0.001,
+                  decay_steps=steps_per_epoch*(EPOCHS/2),
+                  decay_rate=2,
+                  staircase=False)
+
+optimizer = tf.keras.optimizers.Adam(lr_schedule)
+
+
+#if args.checkpoint is None:
+dt = datetime.datetime.today().strftime("%Y-%b-%d-%H-%M-%S")
+checkpoint_dir = './seq2seq/training_checkpoints-' + dt
+#else:
+#    checkpoint_dir = args.checkpoint
+
+checkpoint_prefix = os.path.join(checkpoint_dir, "ckpt")
+checkpoint = tf.train.Checkpoint(optimizer=optimizer,
+                                  encoder=encoder,
+                                  decoder=decoder)
+#if args.checkpoint is not None:
+#    # restore last model
+#    print("Checkpoint being restored: ", tf.train.latest_checkpoint(checkpoint_dir))
+#    chkpt_status = checkpoint.restore(tf.train.latest_checkpoint(checkpoint_dir))
+#    chkpt_status.assert_existing_objects_matched()  # to check loading worked
+#else:
+print("Starting new training run from scratch")
+
+print("New checkpoints will be stored in: ", checkpoint_dir)
+
+
+
+
+loss_object = tf.keras.losses.SparseCategoricalCrossentropy(
+                    from_logits=False, reduction='none')
+
+def loss_function(real, pred):
+    mask = tf.math.logical_not(tf.math.equal(real, 0))
+    loss_ = loss_object(real, pred)
+
+    mask = tf.cast(mask, dtype=loss_.dtype)
+    loss_ *= mask
+
+    return tf.reduce_mean(loss_)
+
+@tf.function
+def train_step(inp, targ, enc_hidden, max_gradient_norm=5):
+    loss = 0
+    
+    with tf.GradientTape() as tape:
+        #print("inside gradient tape")
+        enc_output, enc_hidden = encoder(inp, enc_hidden)
+        #print("****** encoder output received!! ******")
+        
+        dec_hidden = enc_hidden
+        dec_input = tf.expand_dims([start] * BATCH_SIZE, 1)
+        
+        # Teacher forcing - feeding the target as the next input
+        for t in range(1, targ.shape[1]):
+            # passing enc_output to the decoder
+            predictions, dec_hidden, _ = decoder(dec_input, dec_hidden, enc_output)
+            
+            loss += loss_function(targ[:, t], predictions)
+            # using teacher forcing
+            dec_input = tf.expand_dims(targ[:, t], 1)
+            
+    batch_loss = (loss / int(targ.shape[1]))
+    
+    variables = encoder.trainable_variables + decoder.trainable_variables
+    gradients = tape.gradient(loss, variables)
+    # Gradient clipping
+    clipped_gradients, _ = tf.clip_by_global_norm(
+                                    gradients, max_gradient_norm)
+    optimizer.apply_gradients(zip(clipped_gradients, variables))
+    return batch_loss
+
+
+
+print("Starting Training. Total number of steps / epoch: ", steps_per_epoch)
+
+for epoch in range(EPOCHS):
+    start_tm = time.time()
+    enc_hidden = encoder.initialize_hidden_state()
+    total_loss = 0
+    for (batch, (art, smry)) in enumerate(train_dataset.take(steps_per_epoch)):
+        batch_loss = train_step(art, smry, enc_hidden)
+        total_loss += batch_loss
+        if batch % 100 == 0:
+            ts = datetime.datetime.now().strftime("%d-%b-%Y (%H:%M:%S)")
+            print('[{}] Epoch {} Batch {} Loss {:.6f}'.format(ts,epoch + 1,
+                                                batch,
+                                                batch_loss.numpy())
+                  )
+    # saving (checkpoint) the model every 2 epochs
+    if (epoch + 1) % 2 == 0:
+        checkpoint.save(file_prefix = checkpoint_prefix)
+    print('Epoch {} Loss {:.6f}'.format(epoch + 1,
+                                      total_loss / steps_per_epoch))
+    
+    print('Time taken for 1 epoch {} sec\n'.format(time.time() - start_tm))
+
+encoder.summary()
+decoder.summary()
+
+
+
+
+
+
+
+
+
+
+
+
